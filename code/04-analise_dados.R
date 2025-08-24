ad87a2432e069ff143f0cd199299c13d0d77bb4c
Leo Yada <leofyada@gmail.com>
2025-08-24 19:44:06 -0300

    Inclui dados de bolsas efetivamente distribuídas

--- code/04-analise_dados.R
+++ code/04-analise_dados.R
@@ -8,6 +8,18 @@ df_regionais <- data.table::fread(here("data", "prata", "df_regionais_limpa.csv"
 df_sisu <- data.table::fread(here("data", "prata", "df_sisu_licenciatura.csv"))
 # Importação da base de oferta e demanda
 df_oferta_demanda <- data.table::fread(here("data", "prata", "df_oferta_demanda.csv"))
+# Importação da base de bolsas distribuídas
+df_bolsas_distribuidas <- data.table::fread(here("data", "prata", "df_bolsas_distribuidas.csv"))
+
+# Inclusão da regional na base de bolsas distribuídas
+df_bolsas_distribuidas_tratada <- df_bolsas_distribuidas %>% 
+  left_join(
+    y=df_regionais, by=c("CO_MUNICIPIO_IES"="co_municipio")
+  ) %>% 
+  select(
+    UF, regional, no_curso_aj_2, n, 
+  ) %>% 
+  rename("sg_uf" = "UF", "no_curso_aj" = "no_curso_aj_2")
 
 # Inclusão da regional de ensino na base do SiSU
 df_sisu_tratada <- df_sisu %>% 
@@ -41,10 +53,22 @@ df_oferta_demanda_tratada <- df_oferta_demanda_tratada %>%
   select(-c(co_uf, no_uf)) %>% 
   filter(no_curso_aj != "computacao")
 
+# Inclusão de bolsas distribuídas
+df_final <- df_oferta_demanda_tratada %>% 
+  left_join(
+    y=df_bolsas_distribuidas_tratada, by=c("regional", "no_curso_aj", "sg_uf")
+  ) %>% 
+  rename("qtd_bolsas_distribuidas"="n") %>% 
+  mutate(
+    qtd_bolsas_distribuidas = ifelse(is.na(qtd_bolsas_distribuidas), 0, qtd_bolsas_distribuidas),
+    bolsas_distribuidas_match_550 = ifelse(qtd_bolsas_distribuidas <= match_550, qtd_bolsas_distribuidas, match_550),
+    bolsas_distribuidas_match_650 = ifelse(qtd_bolsas_distribuidas <= match_650, qtd_bolsas_distribuidas, match_650)
+  )
+
 # Exporta base final para análise
-writexl::write_xlsx(df_oferta_demanda_tratada, path=here("data", "ouro", glue("{Sys.Date()}-df_analise_final.xlsx")))
+writexl::write_xlsx(df_final, path=here("data", "ouro", glue("{Sys.Date()}-df_analise_final.xlsx")))
 
 # Remove arquivos
-rm(df_regionais, df_sisu, df_oferta_demanda, df_sisu_tratada, df_oferta_demanda_tratada)
-
+rm(df_regionais, df_sisu, df_oferta_demanda, df_sisu_tratada, df_oferta_demanda_tratada, df_bolsas_distribuidas, df_bolsas_distribuidas_tratada, df_final)
+gc()
 
