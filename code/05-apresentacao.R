a3bca7f42b6cbe6a64db512c4aa3849106be8e8d
Leo Yada <leofyada@gmail.com>
2025-08-24 22:34:40 -0300

    WIP

---
+++ code/05-apresentacao.R
@@ -0,0 +1,8 @@
+#-------------------------------------------
+#- 1-) Gráficos utilizados na apresentação - 
+#-------------------------------------------
+
+# Importa o arquivo final
+df_analise_final <- readxl::read_xlsx(path = here("data", "ouro", glue("{Sys.Date()}-df_analise_final.xlsx")))
+
+
